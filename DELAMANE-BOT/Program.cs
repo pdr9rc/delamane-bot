b192ae1c5170aa04351e66adcdc473134dc8d846
pdr9rc <pedro.gracio@3coresec.com>
2021-06-09 17:15:48 +0100

    wip
    wip

--- DELAMANE-BOT/Program.cs
+++ DELAMANE-BOT/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
+using Amazon;
 using Amazon.LexModelBuildingService;
 using Amazon.LexModelBuildingService.Model;
 using Amazon.Runtime;
 using Newtonsoft.Json;
 
-namespace Delamane_Bot
+namespace DELAMANE_BOT
 {
     class Program
     {
@@ -13,7 +14,7 @@ namespace Delamane_Bot
         {
             PutBotRequest delamane = JsonConvert.DeserializeObject<PutBotRequest>(File.ReadAllText("delamane.json"));
             var awsCredentials = new BasicAWSCredentials(args[0], args[1]);
-            var lex = new AmazonLexModelBuildingServiceClient(awsCredentials);
+            var lex = new AmazonLexModelBuildingServiceClient(awsCredentials, RegionEndpoint.EUCentral1);
             string[] files = Directory.GetFiles("Intents", "*.json", SearchOption.AllDirectories);
             foreach (string file in files)
                 delamane.Intents.Add(JsonConvert.DeserializeObject<Intent>(File.ReadAllText(file)));
