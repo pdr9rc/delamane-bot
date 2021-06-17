25b884673cf8c3ddd5572eb46d4ae605482d159c
c-ctkd <cmcm1995@hotmail.com>
2021-06-17 14:58:10 +0100

    wip: added lex bot input/fullfil validators

---
+++ VALIDATORS/DEFAULT/Function.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.LexEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace DEFAULT
+{
+    public class Function
+    {
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public LexResponse FunctionHandler(LexEvent lexEvent, ILambdaContext context)
+        {
+            var topicArn = lexEvent.SessionAttributes.Where(attr => attr.Key == "TopicArn");
+            if (topicArn.Count() <= 0)
+            {
+                return FullfillIntent(new Dictionary<string, string>(lexEvent.SessionAttributes));
+            }
+            return ElicitIntent(new Dictionary<string, string>(lexEvent.SessionAttributes));
+        }
+
+        //TODO: move this to common utils
+        public LexResponse ElicitIntent(Dictionary<string, string> sessionAttr)
+        {
+            return new LexResponse
+            {
+                SessionAttributes = sessionAttr,
+                DialogAction = new LexResponse.LexDialogAction
+                {
+                    Type = "ElicitIntent",
+                    Message =
+                    {
+                        ContentType = "PlainText",
+                        Content = "Some Prompt to trigger other intent"
+                    }
+                }
+            };
+        }
+
+
+        public LexResponse FullfillIntent(Dictionary<string, string> sessionAttr)
+        {
+            return new LexResponse
+            {
+                SessionAttributes = sessionAttr,
+                DialogAction = new LexResponse.LexDialogAction
+                {
+                    Type = "Delegate",
+                    Slots = new Dictionary<string, string>()
+                }
+            };
+        }
+    }
+}
